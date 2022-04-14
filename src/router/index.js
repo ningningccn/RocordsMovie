7ae97a4c274e83fd837673f9fca3cb3894f0d613
Ning Chau <ningningccn@gmail.com>
2022-04-15 01:45:20 +0800

    git commit -m "update `date +'%Y-%m-%d %H:%M:%S'`";

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 // import Home from '@/views/Home.vue'
 const routes = [
   {
@@ -95,7 +95,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   linkActiveClass: 'active',
   routes
 })
